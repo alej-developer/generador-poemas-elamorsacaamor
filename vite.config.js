5bd7a498ea5bb259f2c7935e04ed9e7587548d8a
alej-developer <alejandropm32@gmail.com>
2026-05-31 16:25:34 +0200

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Esta ruta base es crucial para que GitHub Pages cargue correctamente los recursos
+  base: '/generador-poemas-elamorsacaamor/',
 })
